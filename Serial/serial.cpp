9e1a76a87b4e6310f7a65cbb6ba4d282a5a6447e
annewx <116973377+annewx@users.noreply.github.com>
2023-11-10 22:17:09 +0800

    Update serial.cpp

--- Serial/serial.cpp
+++ Serial/serial.cpp
@@ -54,13 +54,14 @@ bool serial::open(){
                 data += "E";
                 sp_blocking_write(serPort, data.c_str(), 19, 0);
                 serial_detect->serData = data;
-                cout<<data<<endl;
+                
             }
             else{
                 data = "AY+000.00P+000.00NE";
                 sp_blocking_write(serPort,data.c_str(),19,0);
                 serial_detect->serData = data;
             }
+            cout<<data<<endl;
 
         }
 
